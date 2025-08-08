bc9fe89ea63c82aeecfcd94d1c66afba11702d85
bbjoum26 <bbijoum26@gmail.com>
2025-08-09 00:04:07 +0900

    파일 및 폴더 업데이트

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if files:
 
         with tab1:
             render_overview_tab(filtered, df)
-ㄴ
+
         with tab2:
             render_top_tab(filtered)
 
@@ -42,7 +42,7 @@ if files:
             render_time_tab(filtered)
 
         with tab4:
-            ingredient_df = calculate_ingredient_usage(df, recipe_folder="recipe")
+            ingredient_df = calculate_ingredient_usage(df, recipe_folder="C:/Users/fishr/vending_dashboard/recipe")
             if not ingredient_df.empty:
                 render_ingredient_tab(ingredient_df, selected_machine)  # ✅ 자판기 이름 넘겨줌
             else:
